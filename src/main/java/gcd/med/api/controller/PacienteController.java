2c31826cc252a30c6e537d0922c4aba23a0a3901
Guilherme C Do <gcd.future@gmail.com>
2023-02-21 09:24:38 -0300

    Created PacienteController's second method. 'listar'

--- src/main/java/gcd/med/api/controller/PacienteController.java
+++ src/main/java/gcd/med/api/controller/PacienteController.java
@@ -1,12 +1,16 @@
 package gcd.med.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import gcd.med.api.paciente.DadosCadastroPaciente;
+import gcd.med.api.paciente.DadosListagemPaciente;
 import gcd.med.api.paciente.Paciente;
 import gcd.med.api.paciente.PacienteRepository;
 import jakarta.transaction.Transactional;
@@ -26,6 +30,13 @@ public class PacienteController {
 		repository.save(new Paciente(dados));
 		
 		System.out.println("Cadastrado com sucesso !");
+	}
+	
+	@GetMapping
+	public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = {"nome"})  org.springframework.data.domain.Pageable paginacao) {
+		
+		return repository.findAll(paginacao).map(DadosListagemPaciente::new);
+		
 	}
 
 }
---
+++ src/main/java/gcd/med/api/paciente/DadosListagemPaciente.java
@@ -0,0 +1,15 @@
+package gcd.med.api.paciente;
+
+public record DadosListagemPaciente(
+		String nome,
+		
+		String email,
+	
+		String cpf	
+		) {
+	
+	public DadosListagemPaciente ( Paciente paciente) {
+		this(paciente.getNome(), paciente.getEmail(), paciente.getCpf());
+	}
+
+}
